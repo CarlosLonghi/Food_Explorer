67b8cdf76773dd15b7fe1f3e97a757ed94e9a9c3
carlos <carloslonghi.cl@gmail.com>
2023-10-03 23:59:20 -0300

    style: refactor style input component

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,33 +1,22 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  width: 100%;
-  
   display: flex;
   align-items: center;
-  text-align: center;
-  margin-bottom: 3rem;
-
+  gap: 0.5rem;
+  border-radius: 0.5rem;
   background-color: ${({theme}) => theme.COLORS.DARK_300};
-  
-  border-radius: 5px;
+  margin-bottom: 3rem;
+  padding: 0 1.5rem;
 
   > input {
-    height: 4.8rem;
     width: 100%;
-
-    padding: 1.2rem 1.4rem;
-
+    
     color: ${({ theme }) => theme.COLORS.LIGHT_100};
     background: transparent;
-    border: 0;
 
     &::placeholder {
       color: ${({ theme }) => theme.COLORS.LIGHT_500};
     }
-  }
-
-  > svg {
-    margin-left: 1.6rem;
   }
 `
