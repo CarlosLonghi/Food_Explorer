f9783c92fb3c8ab3d93e1a8c23258f5699caed56
carlos <carloslonghi.cl@gmail.com>
2023-11-09 22:03:49 -0300

    refactor: add NotFound page in admin routes

--- src/routes/admin.routes.jsx
+++ src/routes/admin.routes.jsx
@@ -4,14 +4,16 @@ import { Home } from '../pages/Home'
 import { Preview } from '../pages/Preview'
 import { New } from '../pages/New'
 import { Edit } from '../pages/Edit'
+import { NotFound } from '../pages/NotFound'
 
 export function AdminRoutes(){
-  return(
+  return( 
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/new' element={<New/>}/>
       <Route path='/preview/:id' element={<Preview/>}/>
       <Route path='/edit/:id' element={<Edit/>}/>
+      <Route path="*" exact={true} element={<NotFound/>}/>
     </Routes>
   )
 }
